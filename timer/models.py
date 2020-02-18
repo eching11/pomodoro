d0d83d4a23ae606ffe18824348b3d28b3fdd5184
Erica Ching <eching11@gmail.com>
2020-02-17 23:33:54 -0800

    Created pomodoro, category, and user models for pomodoro using django framework

---
+++ timer/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class User(models.Model):
+    """Model representing a user."""
+    userID = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique identifier for user')
+    username = models.EmailField(help_text='Email address')
+    
+
+class Category(models.Model):
+    """Model representing a pomodoro category."""
+    name = models.CharField(max_length=100, help_text='Enter the pomodoro category')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+# reverse used to generate URLs by reversing the URL patterns
+from django.urls import reverse
+
+class Pomodoro(models.Model):
+    """Model representing a pomodoro (but not specific pomodoro)."""
+    task_name = models.TextField(blank=False, help_text='Enter task name for pomodoro')
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular pomodoro across whole library')
+
+    # Foreign Key used bc pomodoro can only have one category but a category can have multiple pomodoros.
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+
+    day_of_week = models.DateField(help_text='Day of week (i.e. Monday-Sunday)')
+    time_of_day = models.DateTimeField(help_text='XX:XX AM or PM')
+    minutes = models.IntegerField(default=25)    
+ 
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.task_name
+
+    def get_absolute_url(self):
+        """Returns the url to access a deail record for this pomodoro."""
+        return reverse('pomodoro-detail', args=[str(self.id)])
