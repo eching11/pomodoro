8df2e3f706e033337f8cf719321b8b21ee292657
Erica Ching <eching11@gmail.com>
2020-02-18 20:40:45 -0800

    created admin site, templates, and able to get data from models

--- timer/models.py
+++ timer/models.py
@@ -1,34 +1,46 @@
 from django.db import models
+from django.urls import reverse # reverse used to generate URLs by reversing the URL patterns
 import uuid
 
 # Create your models here.
-class User(models.Model):
-    """Model representing a user."""
-    userID = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique identifier for user')
-    username = models.EmailField(help_text='Email address')
-    
+#class User(models.Model):
+#    """Model representing a user."""
+#    userID = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique identifier for user')
+#    username = models.EmailField(help_text='Email address')
+#    verified = models.BooleanField(default=False, help_text='If True, user has been verified via email') 
 
 class Category(models.Model):
     """Model representing a pomodoro category."""
     name = models.CharField(max_length=100, help_text='Enter the pomodoro category')
-
+    categoryID = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, help_text='Unique identifier for category')
+    description = models.TextField(max_length=250, default="", help_text='Brief description')
+    
     def __str__(self):
         """String for representing the Model object."""
         return self.name
+    def display_category(self):
+        """Create a string for the Category. This is required to display category in Admin."""
+        return ', '.join(category.name for category in self.category.all()[:3])
 
-# reverse used to generate URLs by reversing the URL patterns
-from django.urls import reverse
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this category."""
+        return reverse('category-detail', args=[str(self.id)])
+
+    display_category.name = 'Category'
+
+
+
 
 class Pomodoro(models.Model):
     """Model representing a pomodoro (but not specific pomodoro)."""
     task_name = models.TextField(blank=False, help_text='Enter task name for pomodoro')
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular pomodoro across whole library')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, help_text='Unique ID for this particular pomodoro across whole library')
 
     # Foreign Key used bc pomodoro can only have one category but a category can have multiple pomodoros.
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    categoryID = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
 
-    day_of_week = models.DateField(help_text='Day of week (i.e. Monday-Sunday)')
+    day_of_week = models.CharField(max_length=10, help_text='Day of week (i.e. Monday-Sunday)')
     time_of_day = models.DateTimeField(help_text='XX:XX AM or PM')
     minutes = models.IntegerField(default=25)    
  
@@ -37,5 +49,5 @@ class Pomodoro(models.Model):
         return self.task_name
 
     def get_absolute_url(self):
-        """Returns the url to access a deail record for this pomodoro."""
+        """Returns the url to access a detail record for this pomodoro."""
         return reverse('pomodoro-detail', args=[str(self.id)])
--- timer/views.py
+++ timer/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
+from timer.models import Category, Pomodoro
+
+def index(request):
+    """ View function for home page of site."""
+    
+    # Generate counts of some of the main objects
+    num_categorys = Category.objects.all().count()
+    num_pomodoros = Pomodoro.objects.all().count()
+    
+    # Pomodoro's for today (Tuesday)
+    num_pomodoros_today = Pomodoro.objects.filter(day_of_week='Tuesday').count()
+    
+    context = {
+        'num_categorys': num_categorys,
+        'num_pomodoros': num_pomodoros,
+        'num_pomodoros_today': num_pomodoros_today,
+    }
+    
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class CategoryListView(generic.ListView):
+    model = Category
+    context_object_name = 'my_category_list' # template variable
+    #queryset = Category.objects.filter(name__icontains='Coding')[:5] # Get 5 categories containing name coding
+    template_name = 'categories/category_list.html' # specifying template name/location
+
+class PomodoroDetailView(generic.DetailView):
+    model = Pomodoro
+
+from django.shortcuts import get_object_or_404
+
+def pomodoro_detail_view(request, primary_key):
+    pomodoro = get_object_or_404(Pomodoro, pk=primary_key)
+    return render(request, 'timer/pomodoro_detail.html', context={'pomodoro': pomodoro})
+
